70a6b831e31c3084d763a03b1b32ecf3218cf832
Asia Valentine <asiavalentine22@gmail.com>
2020-12-01 18:19:11 -0500

    background image changes to nighttime

--- starter-code/time.js
+++ starter-code/time.js
@@ -12,32 +12,36 @@ var fullTime = hour + ":" + minutes;
 document.getElementById('currentTime').innerHTML = fullTime;
 
 if (hour < 12) {
-    document.getElementById('greeting-text').innerHTML = 'GOOD MORNING,'
+    document.getElementById('greeting-text').innerHTML = 'GOOD MORNING'
 }
 else if (hour >= 12 && hour < 18) {
-    document.getElementById('greeting-text').innerHTML = 'GOOD AFTERNOON,'
+    document.getElementById('greeting-text').innerHTML = 'GOOD AFTERNOON'
 }
 else {
     document.getElementById('moon-icon').style.display = 'inline-block'
     document.getElementById('sun-icon').style.display = 'none'
-    document.getElementById('greeting-text').innerHTML = 'GOOD EVENING,'
-    document.getElementsByTagName('body')[0].style.backgroundImage = "url('./assets/mobile/bg-image-nighttime.jpg')"
-    document.getElementById('info-box').style.backgroundColor
+    document.getElementById('greeting-text').innerHTML = 'GOOD EVENING'
+    document.getElementById('info').style.backgroundColor = 'rgba(0,0,0, 0.75)'
+    document.getElementById('info').style.color = 'white'
+    //change background to nighttime
+    document.getElementsByTagName('body')[0].classList.add('nighttime')
+    
 }
 
-var moreLessBtn = document.getElementById('info-box');
+var moreInfoBox = document.getElementById('info');
 var toggleBtnText = document.getElementById('more-less').children[0];
 var arrowIcon = document.getElementById('arrow-icon');
 var quoteContainer = document.getElementById('quote-container');
 
 const toggleBtn = () => {
-    if (moreLessBtn.style.display == 'inline-flex') {
-        moreLessBtn.style.display = 'none'
+    if (moreInfoBox.style.display == 'inline-flex') {
+        moreInfoBox.style.display = 'none'
         toggleBtnText.innerHTML = 'MORE'
+        arrowIcon.style.transform = 'rotate(360deg)'
         quoteContainer.style.display = 'flex'
     }
     else {
-        moreLessBtn.style.display = 'inline-flex'
+        moreInfoBox.style.display = 'inline-flex'
         toggleBtnText.innerHTML = 'LESS'
         arrowIcon.style.transform = 'rotate(180deg)'
         quoteContainer.style.display = 'none'
